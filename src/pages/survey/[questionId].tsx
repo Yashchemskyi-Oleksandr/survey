07451317e433cf98cf5746d44092ea99ee15e587
Oleksandr Yashchemskyi <your email>
2024-11-25 20:26:56 +0200

    - added getStaticProps and getStaticPaths (prepared data for component)

---
+++ src/pages/survey/[questionId].tsx
@@ -0,0 +1,39 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import SurveyLayout from '@/components/layouts/SurveyLayout';
+import { QuestionProps } from '@/types/surveyTypes';
+import surveyConfig from '../../config/survey.json';
+
+const QuestionPage = ({ currentQuestion, initialQuestionId }: QuestionProps) => (
+  <div>
+    {JSON.stringify(initialQuestionId)}
+    {JSON.stringify(currentQuestion)}
+  </div>
+);
+
+export default QuestionPage;
+
+// should use api for getStaticPaths and getStaticProps
+// Generate static paths for all questions
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = surveyConfig.questions.map((question) => ({
+    params: { questionId: question.id },
+  }));
+
+  return { paths, fallback: false };
+};
+
+// Fetch question data based on the questionId
+export const getStaticProps: GetStaticProps = async (context) => {
+  const questionId = context.params?.questionId;
+  const question = surveyConfig.questions.find((q) => q.id === questionId);
+
+  if (!question) {
+    return { notFound: true };
+  }
+
+  return {
+    props: { currentQuestion: question, initialQuestionId: surveyConfig.questions[0].id },
+  };
+};
+
+QuestionPage.getLayout = (page: React.ReactNode) => <SurveyLayout>{page}</SurveyLayout>;
