32269d37790f1276cc8888eb991076233b3a21d5
Oleksandr Yashchemskyi <your email>
2024-11-25 23:29:13 +0200

    - connected toastify

--- src/components/layouts/GlobalLayout.tsx
+++ src/components/layouts/GlobalLayout.tsx
@@ -1,12 +1,20 @@
 import { store } from '@/store/store';
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 type GlobalLayoutProps = {
   children: React.ReactNode;
 };
 
 const GlobalLayout: React.FC<GlobalLayoutProps> = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
+  <Provider store={store}>
+    {children}
+    <ToastContainer
+      closeOnClick
+      limit={3}
+    />
+  </Provider>
 );
 
 export default GlobalLayout;
